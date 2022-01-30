e15845059f5721eb81b794b0bc8222ca728f6474
choi1087 <adcdm87@gmail.com>
2022-01-30 16:43:32 +0900

    first commit

---
+++ src/test/java/com/choitaek/meethere/meethere/MeethereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choitaek.meethere.meethere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeethereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
